369978d21446b2e668109b31a0e6dcf9c7b99b20
mor <srgark@gmail.com>
2016-05-10 10:54:12 +0200

    formato bootstrap de la ficha, redonditas de nivel

--- views/public/javascripts/sheet.js
+++ views/public/javascripts/sheet.js
@@ -1,38 +1,52 @@
 /**
  * Script para manejar la tabla de la ficha de personaje
+ * - TODO funciones de interactividad con el usuario (subir las estadísticas de nivel clicando en las bolas grises, o bajarlas clicando en las rojas)
  * Created by mor on 9/05/16.
  */
-//var util = util();
 
-var char;
+var char; // personaje alojado en la memoria del cliente (es decir, el que está jugando)
+var icons = {
+    set: "set_level_icon.png",
+    unset: "unset_level_icon.png",
+    maxable: "max_level_icon.png"
+};
 
+/**
+ * Abre un elemento desplegable
+ * @param selectorId ID del elemento (string)
+ */
 function openNav(selectorId) {
     var element = $("#"+selectorId);
-    element.css('width', '100%');
+    element.fadeIn('fast', function() {element.css('width', '100%')});
 }
+/**
+ * Cierra un elemento desplegable
+ * @param selectorId ID del elemento (string)
+ */
 function closeNav(selectorId) {
     var element = $("#"+selectorId);
-    element.css('width', '0%');
+    element.fadeOut('fast', function() {element.css('width', '0%')});
 }
-
+/**
+ * Mediante recursividad, construye una tabla a partir de un objeto de estadísticas. Si el objeto es un array, dibujará
+ * una tabla de una fila para cada posición. Si es un objeto de estadísticas
+ * @param statsObj
+ * @returns {*}
+ */
 function table(statsObj) {
-    var name;
-    if(statsObj.hasOwnProperty('name')) name = statsObj.name;
-    if(util.is(util.stat, statsObj)) {
-        return "<tr><td>"+statsObj.name+"</td><td id='"+statsObj.name+"'>"+statsObj.level+"</td></tr>";
-    }
-    if(util.is(util.table, statsObj)) {
+    if(util.is(util.stat, statsObj)) { // es un objeto de estadística singular
+        return "<tr><td>"+statsObj.name+"</td><td id='"+statsObj.name+"'>"+level(statsObj)+"</td></tr>";
+    } else if(util.is(util.table, statsObj)) { // es un objeto de conjunto de estadísticas
         var subTable = "", stats = statsObj.stats;
         var tableAdd = function(a) {subTable += a}, len = stats.length;
-        tableAdd("<td><table id='"+name+"'><thead><th>"+name+"</th></thead><tbody><tr>");
+        tableAdd("<td><div class='table-hover'><table id='"+statsObj.name+"' class='table'><thead><th>"+statsObj.name+"</th></thead><tbody><tr>");
         for(var i = 0; i<len; i++) {
             var cur = stats[i];
             tableAdd("<td>"+table(cur)+"</td>");
         }
-        tableAdd("</tr></tbody></table></td>");
+        tableAdd("</tr></tbody></table></div></td>");
         return subTable;
-    }
-    if(util.type(util.arr, statsObj)) {
+    } else if(util.type(util.arr, statsObj)) { // es un array
         var mainTable = $("table#stats>tbody");
         var content = "";
         var contentAdd = function(a) {content += a};
@@ -41,9 +55,23 @@ function table(statsObj) {
     }
 }
 
+function level(stat) { // OMG OMG OMG IT WORKS!!!! :D :D :D :D so happy 4 dat ^^
+    var ret = "", level = stat.level;
+    var icon, max = stat.max;
+    // bucle que inserta la imagen de una esfera de nivel. Si el personaje posee ese nivel aprendido, sera roja, y sino, gris
+    for(var i = 1; i<=10; i++) {
+        if(stat.hasOwnProperty('max'))  icon = i<=max ? icons.maxable : icons.unset;
+        else if(i>level) icon = icons.unset;
+        else icon = icons.set;
+        ret += "<img src='/public/images/icon/" + icon/*(i>level ? "un"+icon : icon)*/ + "'>";
+    }
+    return ret;
+}
+
 $("table#stats").ready(function() {
     var charJSON = $("div#char").text();
     char = JSON.parse(charJSON);
+    char.stats[3].stats[2].max = 3;
     table(char.stats);
 });
 
@@ -68,4 +96,4 @@ if (window["WebSocket"]) {
         }
         connect();
     });
-};
+}
