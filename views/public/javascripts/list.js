7b3bb4e5576f6db667baa7346ac921b2b5f1cbd5
mor <srgark@gmail.com>
2016-05-24 20:34:27 +0200

    i feel the joy joy joy joy inside in my sockets

---
+++ views/public/javascripts/list.js
@@ -0,0 +1,29 @@
+/**
+ * Objeto para controlar la lista de jugadores
+ * Created by mor on 24/05/16.
+ */
+
+var list = {
+    load: function () {
+        var element = $('select#char-list'), charList = game.charList, npcList = game.npcList;
+        list.appendList(element, charList);
+        list.appendList(element, npcList);
+        element.change(function () {
+            button.charSelectOptionClick($(this).children(':selected'));
+        });
+    },
+    appendList: function (element, list) {
+        var childs = "";
+        var addChild = function (c) {
+                childs += c
+            },
+            newChild = function (n) {
+                return "<option>" + n + "</option>"
+            };
+        list.forEach(function (ch) {
+            var charName = charFunctions.findData(ch, 'nombre').value;
+            addChild(newChild(charName));
+        });
+        element.append(childs);
+    }
+};
